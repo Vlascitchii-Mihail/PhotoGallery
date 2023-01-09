db39325d100f9c973488d4fa36a79f3b9b776b09
Marty McFly <99603411+Vlascitchii-Mihail@users.noreply.github.com>
2023-01-09 17:48:46 +0200

    Delete app directory

--- app/src/main/java/com/bignerdrunch/photogallery/api/FlickrResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.bignerdrunch.photogallery.api
-
-import com.squareup.moshi.JsonClass
-
-/**
- * Will be compared with external JSON's object
- */
-@JsonClass(generateAdapter = true)
-data class FlickrResponse (val photos: PhotoResponse)
